096936fb2ab41d519167ecb4578b45b26523fe31
Ali Farooq <alifarooq@csus.edu>
2026-01-07 17:12:49 -0800

    Initial Spring Boot setup with home page

---
+++ src/main/java/com/kingstire/invoice/KingsTireInvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.kingstire.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsTireInvoiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingsTireInvoiceAppApplication.class, args);
+	}
+
+}
